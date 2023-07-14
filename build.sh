fd9e6d2a2a8ac9e0560d16d7f77031d4a36261c1
Raul Max <>
2023-07-14 12:52:51 -0300

    chore: proj

--- build.sh
+++ build.sh
@@ -6,35 +6,37 @@ rm -rf .build
 rm -rf build
 rm -rf .archive
 rm -rf .derivedData
+rm -rf $NAME.xcodeproj
 rm -rf $NAME.xcframework
 rm -rf *.zip
 
 swift build
 swift build -c release
-
 xcodegen
 
 xcodebuild archive \
   -project $NAME.xcodeproj \
   -scheme $NAME \
+  -configuration release \
   -archivePath .archive/ios.xcarchive \
   -derivedDataPath .derivedData \
-  -destination "generic/platform=iOS" \
+  -destination generic/platform=iOS \
   -sdk iphoneos \
   BUILD_DIR=.build \
   SKIP_INSTALL=NO \
-  BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+  BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild archive \
   -project $NAME.xcodeproj \
   -scheme $NAME \
+  -configuration release \
   -archivePath .archive/ios-simulator.xcarchive \
   -derivedDataPath .derivedData \
   -destination "generic/platform=iOS simulator" \
   -sdk iphonesimulator \
   BUILD_DIR=.build \
   SKIP_INSTALL=NO \
-  BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+  BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework \
   -framework .archive/ios.xcarchive/Products/Library/Frameworks/$NAME.framework \
